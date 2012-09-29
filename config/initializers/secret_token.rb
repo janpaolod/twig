a0ec77644e398f4d60a092fa06e4c41ffa8d4ff9
Jp <janpaolod@gmail.com>
2012-09-29 21:42:20 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twig::Application.config.secret_token = '5d77a861b85cc6440d80010a95c20c6c68565d3a6a0472b7b5087e4cc7036f690723573faddfbd0fcb2c96844630891a9e6984a8491761ef1962d200e94bbc35'
